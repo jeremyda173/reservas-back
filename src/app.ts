28a098e2e9399c94052aa29ede0fef6d82485387
SRTENEBROSO00 <kristhian2800@gmail.com>
2025-07-22 21:48:09 -0400

    feat: implement table management routes and controller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import user_router from "./routes/user.route"
+import table_router from "./routes/table.route"
 
 const app = express();
 app.use(cors({
@@ -12,4 +13,5 @@ app.get("/", (_, res) => {
     res.send("ApI funcionando correctamente con Typescript y firebase");
 });
 app.use("/user", user_router)
+app.use('/table', table_router)
 export default app;
