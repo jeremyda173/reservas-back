ec26e78f4d64c975e8ae36cd7c57f16077a211a6
jeremyda173 <jeremygithud173@gmail.com>
2025-09-13 14:57:22 -0400

    feat: improve formatting and consistency in table route definitions

--- src/routes/table.route.ts
+++ src/routes/table.route.ts
@@ -1,19 +1,37 @@
 import { Router } from 'express';
 import { TableController } from '../controllers/table.controller';
 
-const router = Router()
-
+const router = Router();
 const controller = new TableController();
 
-//Crear mesa
+// Crear mesa
 router.post(
-    '/create',
-    controller.createTable.bind(controller)
-)
-//Obtener todas las mesas
+  '/create',
+  controller.createTable.bind(controller)
+);
+
+// Obtener todas las mesas
 router.get(
-    '/',
-    controller.getAllTables.bind(controller)
-)
+  '/',
+  controller.getAllTables.bind(controller)
+);
+
+// Obtener una mesa por id
+router.get(
+  '/:id',
+  controller.getTableById.bind(controller)
+);
+
+// Actualizar una mesa por id
+router.put(
+  '/:id',
+  controller.updateTable.bind(controller)
+);
+
+// Eliminar una mesa por id
+router.delete(
+  '/:id',
+  controller.deleteTables.bind(controller)
+);
 
 export default router;
