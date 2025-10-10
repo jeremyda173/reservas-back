f34137b4745efa946ba6c659aa7bc77e5b8dd02f
jeremyda173 <jeremygithud173@gmail.com>
2025-10-10 19:07:46 -0400

    fix: corregir URL de CORS para localhost

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 //Cors 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost/5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   methods:  ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true
 }))
